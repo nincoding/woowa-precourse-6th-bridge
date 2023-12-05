d90e098f11d2599355c1e9213a9cb6b70c25e688
한수지 <fatholic5@gmail.com>
2023-12-05 20:58:35 +0900

    :sparkles: feat: 다리게임 기능구현

--- src/domains/BridgeGame.js
+++ src/domains/BridgeGame.js
@@ -6,6 +6,8 @@ import BridgeRandomNumberGenerator from '../BridgeRandomNumberGenerator.js';
  */
 class BridgeGame {
   #bridge;
+  #userMoveStatus = [];
+  #tryCount;
 
   constructor(size) {
     this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
@@ -16,14 +18,34 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(index, moving) {
+    const isMoving = this.#bridge[index] === moving;
+    const status = isMoving ? 'O' : 'X';
+
+    this.#userMoveStatus.push({ [moving]: status });
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#tryCount += 1;
+    this.#userMoveStatus = [];
+  }
+
+  isClear() {
+    return !this.#userMoveStatus.some((part) => Object.values(part).includes('X'));
+  }
+
+  getTryCount() {
+    return this.#tryCount;
+  }
+
+  getUserMoveStatus() {
+    return this.#userMoveStatus;
+  }
 }
 
 export default BridgeGame;
