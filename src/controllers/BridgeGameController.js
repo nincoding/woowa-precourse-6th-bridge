26fc16c847ddd11b1fe80019f0ddcbfe4cc3413d
한수지 <fatholic5@gmail.com>
2023-12-05 21:17:38 +0900

    :sparkles: feat: 최상단 App과 컨트롤러 연결

---
+++ src/controllers/BridgeGameController.js
@@ -0,0 +1,11 @@
+import BridgeGame from '../domains/BridgeGame.js';
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+
+class BridgeGameController {
+  constructor() {}
+
+  async start() {}
+}
+
+export default BridgeGameController;
