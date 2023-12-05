0714e1ea6756419b66881f86086b9daf4f8f1691
한수지 <fatholic5@gmail.com>
2023-12-05 19:16:57 +0900

    :sparkles: feat: 입력기능 구현 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,6 @@
+import Validator from '../validators/Validator.js';
+import { Console } from '@woowacourse/mission-utils';
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,17 +8,37 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  async readBridgeSize() {},
+  async readBridgeSize() {
+    const bridgeSize = await Console.readLineAsync('다리의 길이를 입력해주세요.\n');
+
+    Validator.validateBridgeSize(bridgeSize);
+
+    return bridgeSize;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  async readMoving() {},
+  async readMoving() {
+    const moving = await Console.readLineAsync('\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n');
+
+    Validator.validateMoving(moving);
+
+    return moving;
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  async readGameCommand() {},
+  async readGameCommand() {
+    const gameCommand = await Console.readLineAsync(
+      '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n'
+    );
+
+    Validator.validateGameCommand(gameCommand);
+
+    return gameCommand;
+  },
 };
 
 export default InputView;
