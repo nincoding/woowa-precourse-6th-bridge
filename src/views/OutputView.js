b2f48f83979e3a372141bb7ba95c58bcc242a0c9
한수지 <fatholic5@gmail.com>
2023-12-05 22:21:27 +0900

    :recycle: refactor: 줄바꿈 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -42,7 +42,7 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(moveStatus, isClear, tryCount) {
-    Console.print('최종 게임 결과');
+    Console.print('\n최종 게임 결과');
     this.printMap(moveStatus);
     Console.print(`게임 성공 여부: ${isClear ? '성공' : '실패'}`);
     Console.print(`총 시도한 횟수: ${tryCount}`);
