1a645755a6c5cf5e7b7f52d61182105e562fb659
한수지 <fatholic5@gmail.com>
2023-12-05 19:08:10 +0900

    :recycle: refactor: 뷰 객체 경로이동 및 입력뷰 비동기 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,17 +5,17 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  async readBridgeSize() {},
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  async readMoving() {},
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  async readGameCommand() {},
 };
 
 export default InputView;
