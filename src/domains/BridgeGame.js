e294711c0877be14dae88374349dc5e7d9648bb0
한수지 <fatholic5@gmail.com>
2023-12-05 20:43:42 +0900

    :sparkles: feat: 다리게임 시작시 자동으로 다리 생성 및 저장

--- src/domains/BridgeGame.js
+++ src/domains/BridgeGame.js
@@ -1,8 +1,15 @@
+import BridgeMaker from '../BridgeMaker.js';
+import BridgeRandomNumberGenerator from '../BridgeRandomNumberGenerator.js';
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  constructor() {}
+  #bridge;
+
+  constructor(size) {
+    this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+  }
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
